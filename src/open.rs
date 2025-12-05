4d8a554ea3dfe568ad0684485e4a1395e6428b9e
Moritz Brödel <moritz.broedel@gmail.com>
2025-12-06 00:43:56 +0200

    Update obj2 dependencies

--- src/open.rs
+++ src/open.rs
@@ -9,17 +9,15 @@ pub trait Open {
 
 impl Open for App {
     fn open(&self) {
-        unsafe {
-            let workspace = NSWorkspace::sharedWorkspace();
-            let bundle_id = NSString::from_str(self.bundle_id.as_str());
-            // TODO use thiserror to define legible errors and return them
-            let app_url = workspace
-                .URLForApplicationWithBundleIdentifier(&bundle_id)
-                .expect("Could not find app with this Bundle ID");
-            // TODO use openApplicationAtUrl (requires async)
-            if !workspace.openURL(&app_url) {
-                panic!("macOS failed to launch the application");
-            }
+        let workspace = NSWorkspace::sharedWorkspace();
+        let bundle_id = NSString::from_str(self.bundle_id.as_str());
+        // TODO use thiserror to define legible errors and return them
+        let app_url = workspace
+            .URLForApplicationWithBundleIdentifier(&bundle_id)
+            .expect("Could not find app with this Bundle ID");
+        // TODO use openApplicationAtUrl (requires async)
+        if !workspace.openURL(&app_url) {
+            panic!("macOS failed to launch the application");
         }
     }
 }
